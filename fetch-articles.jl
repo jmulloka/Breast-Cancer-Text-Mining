22fe24ca68eb32da509291a93d374031149a4997
kimaina <kimkenal@gmail.com>
2018-04-10 17:06:42 -0400

    Fixed fetch article function

--- fetch-articles.jl
+++ fetch-articles.jl
@@ -18,17 +18,17 @@ function fetchBreastCancerArticles(cancerType="breast cancer", researchType="dia
     #print(search_result)
 
     # retrieve the pmids into an array
-    pmid_array = []
+    pmids = []
     for result_line in split(search_result, "\n")
       # get list of pmids
       pmid = match(r"<Id>(\d+)<\/Id>", result_line)
       if pmid != nothing
-        push!(pmid_array,pmid[1])
+        push!(pmids,pmid[1])
       end
     end
 
     # concatenate pmids into a single comma separated string
-    id_string = join(collect(pmid_array), ",")
+    id_string = join(collect(pmids), ",")
 
     # define base fetch query for eutils
     base_fetch_query = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
@@ -43,52 +43,66 @@ function fetchBreastCancerArticles(cancerType="breast cancer", researchType="dia
     close(output_file)
 
     # field set
+    pmid_array = [] # title set
     title_array = [] # title set
     abstract_array=[] # article abstract
     date_created_array=[] # Create Date (CRDT).
     # read through each line
-    for fetch_line in split(fetch_result, "\n")
+    for fetch_article in split(fetch_result, "PMID- ")
+        # clear all field
+        pmid=""
+        full_title=""
+        full_abstract=""
+        date_created=""
 
-      # get Title
-      title = match(r"TI  - ([\w\W \r\n]+)", fetch_line)
-      if title != nothing
-          full_title=  title[1]
-          push!(title_array,full_title)
-          #println("$full_title")
-      end
+        # extract PMID
+        pmid_str = match(r"([0-9]+)", fetch_article)
+        if pmid_str != nothing
+          pmid=pmid_str[1]
+        end
 
-      # get abstract
-      abstract_str = match(r"AB  - ([\w\W \r\n]+)", fetch_line)
-      if abstract_str != nothing
-          push!(abstract_array, abstract_str[1])
-        #println("$(abstract_str[1])")
-      end
 
-      # date created
-      date_created_str = match(r"CRDT- ([\w\W \r\n]+)", fetch_line)
-      if date_created_str != nothing
-          push!(date_created_array, date_created_str[1])
-        #println("$(abstract_str[1])")
-      end
+        for fetch_line in split(fetch_article, "\n")
+          # get Title
+          title = match(r"TI  - ([\w\W \r\n]+)", fetch_line)
+          if title != nothing
+              full_title=title[1]
+          end
+
+          # get abstract
+          abstract_str = match(r"AB  - ([\w\W \r\n]+)", fetch_line)
+          if abstract_str != nothing
+              full_abstract=abstract_str[1]
+          end
+
+          # date created
+          date_created_str = match(r"CRDT- ([\w\W \r\n]+)", fetch_line)
+          if date_created_str != nothing
+              date_created=date_created_str[1]
+          end
+        end
+
+        # push everything
+        if pmid != ""
+            push!(pmid_array, pmid)
+            push!(title_array, full_title)
+            push!(abstract_array, full_abstract)
+            push!(date_created_array, date_created)
+        end
     end
-
-    println("pmid_array: $(length(pmid_array))")
-    println("title_array: $(length(title_array))")
-    println("abstract_array: $(length(abstract_array))")
-    println("date_created_array: $(length(date_created_array))")
-
 
     # return dictionary of pmid and title
     return(DataFrame(pmid=pmid_array,
                     title=title_array,
-                    date_created=date_created_array
+                    date_created=date_created_array,
+                    main_abstract=abstract_array
                     )
             )
 
 end
 
 # usage
-#df = fetchBreastCancerArticles("breast cancer", "diagnosis",2015,2018)
+#df = fetchBreastCancerArticles("breast cancer", "diagnosis",2000,2018)
 
 # ref
 #https://www.nlm.nih.gov/bsd/mms/medlineelements.html#ab
